cff7b855dd2592fd946b88cf509dde42401f4599
GeorgieSantiago <milestoneartisan@gmail.com>
2018-01-08 21:38:34 -0500

    Form post are routed

--- routes/web.php
+++ routes/web.php
@@ -18,6 +18,8 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
-
+Route::post('/home' , 'HomeController@createTables')->name('init');
 Route::resource('/configure' , 'ConfigurationController');
+
+
 
